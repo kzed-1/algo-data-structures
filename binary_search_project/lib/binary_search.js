60079a9afde5ae7f13012493480dc98c7bad2be8
Kzhao <ken.zh77@gmail.com>
2020-01-14 16:16:47 -0500

    start linked list

---
+++ binary_search_project/lib/binary_search.js
@@ -0,0 +1,38 @@
+function binarySearch(array, target) {
+    if (array.length === 0) return false;
+
+    let midIdx = Math.floor(array.length/2);
+    let left = array.slice(0, midIdx);
+    let right = array.slice(midIdx + 1)
+
+    if(array[midIdx] < target) {
+        return binarySearch(right, target)
+    }else if (array[midIdx] > target) {
+        return binarySearch(left, target)
+    }else {
+        return true 
+    }
+
+}
+
+function binarySearchIndex(array, target, lo=0, hi = array.length-1) {
+    if (lo === hi) return -1;
+
+    let midIdx = Math.floor((lo + hi) / 2);
+
+
+    if(array[midIdx] < target){
+        return binarySearchIndex(array, target, midIdx + 1, hi)
+    }else if (array[midIdx] > target) {
+        return binarySearchIndex(array, target, lo, midIdx)
+    }else {
+        return midIdx 
+    }
+
+}
+
+
+module.exports = {
+    binarySearch,
+    binarySearchIndex
+};

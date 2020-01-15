076df78589c5c8590bb4c2fd621522390ac0ff35
Kzhao <ken.zh77@gmail.com>
2020-01-15 14:09:15 -0500

    linked list

--- linked_list_project/lib/linked_list.js
+++ linked_list_project/lib/linked_list.js
@@ -59,38 +59,88 @@ class LinkedList {
     // TODO: Implement the removeTail method here
     removeTail() {
         if (this.length === 0) return undefined;
+        if (this.tail == this.head) {
+            let temp = this.head
+            this.tail = null;
+            this.head = null;
+            this.length --;
+            return temp;
+        }
         this.tail.next = null;
 
-        let next = this.head.next
+        let next = this.head
         
         while (next) {
-            next;
+            if (next.next === this.tail) break;
+            next = next.next 
         }
-
-       
-   
-
+        let returnTail = this.tail;
+        next.next = null;
+        this.tail = next;
+        this.length --;
+        return returnTail
 
     }
 
     // TODO: Implement the addToHead method here
     addToHead(val) {
-
+        const newNode = new Node(val)
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+            this.length ++;
+        }else {
+            newNode.next = this.head;
+            this.head = newNode;
+            this.length ++;
+        }
+        return {
+            head: this.head,
+            tail: this.tail, 
+            length: this.length
+        }
     }
 
     // TODO: Implement the removeHead method here
     removeHead() {
-
+        if (this.length === 0){
+            return undefined;
+        }else if (this.length === 1){
+            let temp = this.head;
+            this.head = null;
+            this.tail = null;
+            this.length --;
+            return temp
+        }else {
+            let temp = this.head
+            this.head = this.head.next 
+            this.length --;
+            return temp;
+        }
     }
 
     // TODO: Implement the contains method here
     contains(target) {
-
+        let next = this.head;
+        while(next) {
+            if (next.value === target) return true;
+            next = next.next;
+        }
+        return false;
     }
 
     // TODO: Implement the get method here
     get(index) {
+        if (this.length-1 < index) return null;
+        let LinkedList = [this.head]
+        let counter = 0
 
+        while (counter < index) {
+            LinkedList.push(LinkedList[counter].next)
+            counter ++;
+        }
+
+        return LinkedList[index]
     }
 
     // TODO: Implement the set method here
@@ -100,12 +150,32 @@ class LinkedList {
 
     // TODO: Implement the insert method here
     insert(index, val) {
+        if (this.length-1 < index)return false;
+        let linkedList = [this.head];
+        let counter = 0;
 
+        while(counter < index) {
+            linkedList.push(linkedList[counter].next);
+            counter++;
+        }
+        let newNode = new Node(val)
+
+        if (linkedList.length===1){
+            linkedList[0].next = newNode
+            newNode.next = linkedList[index];
+            this.length ++;
+            return true
+        } else {
+            linkedList[index-1].next = newNode;
+            newNode.next = linkedList[index];
+            this.length ++;
+            return true
+        }
     }
 
     // TODO: Implement the remove method here
     remove(index) {
-
+        
     }
 
     // TODO: Implement the size method here

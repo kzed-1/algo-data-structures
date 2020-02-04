d9c0c05468d9fc3523fd6a49582e7ca462d10f68
Kzhao <ken.zh77@gmail.com>
2020-02-04 17:54:27 -0500

    finished queue

--- queue_project/lib/queue.js
+++ queue_project/lib/queue.js
@@ -21,11 +21,55 @@
 // -----------
 
 class Node {
+    constructor(val, next){
+        this.value = val,
+        this.next = next
+    }
 
 }
 
 class Queue {
+    constructor() {
+        this.front = null,
+        this.back = null, 
+        this.length = 0;
+    }
 
+    enqueue (val) {
+        let node = new Node(val)
+        if(!this.front){
+            this.front = node 
+            this.back = this.front
+        }else {
+            this.back.next = node
+            this.back = node
+        }
+        this.length ++;
+        return this.length
+    }
+
+    dequeue () {
+        if (!this.front) return null;
+        
+        let temp ;
+        if(this.length === 1){
+            temp = this.front;
+            this.front = null;
+            this.back = null;
+            this.length --;
+            return temp.value
+        }
+
+        temp = this.front
+        this.front = temp.next 
+        this.length --;
+        return temp.value
+
+    }
+
+    size () {
+        return this.length
+    }
 }
 
 exports.Node = Node;

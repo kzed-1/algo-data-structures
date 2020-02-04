c14fae71dba275ea58ed9ef26e37c4f9fa929222
Kzhao <ken.zh77@gmail.com>
2020-02-04 17:18:48 -0500

    finished stack

--- stack_project/lib/stack.js
+++ stack_project/lib/stack.js
@@ -21,10 +21,61 @@
 // -----------
 
 class Node {
+    constructor (val, next = null){
+        this.next = next,
+        this.value = val
+    }
 
 }
 
 class Stack {
+    constructor () {
+        this.bottom = null,
+        this.top = null,
+        this.length = 0;
+    }
+
+    push (val) {
+        const node = new Node(val)
+
+        if (this.length === 0) {
+            this.bottom = node 
+            this.top = node
+            this.length ++;
+            return this.length
+        }else {
+            let temp = this.top
+            this.top = node 
+            this.top.next = temp
+            this.length ++;
+            return this.length
+        }
+
+    }
+
+    pop () {
+        if(this.length === 0) return null;
+
+        let temp;
+
+        if (this.length === 1){
+            temp = this.bottom
+            this.bottom = null
+            this.top = null;
+            this.length --;
+        }else {
+            temp = this.top
+            this.top = temp.next
+            temp.next = null
+            this.length --;
+        }
+        return temp.value
+    }
+
+    size () {
+        return this.length
+    }
+
 
 }
 

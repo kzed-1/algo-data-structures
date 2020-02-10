908b6e9713c1d8cbeaf10db85548e58a1f8ca4f7
Kzhao <ken.zh77@gmail.com>
2020-02-10 14:28:54 -0500

    finished tree_node and tree_order

--- tree_order_project/lib/tree_order.js
+++ tree_order_project/lib/tree_order.js
@@ -1,8 +1,23 @@
 function inOrderArray(root) {
+    const result = [];
 
+    if (!root) return result;
+
+    const left = result.concat(inOrderArray(root.left));
+    const right = result.concat(inOrderArray(root.right));
+
+    return result.concat(left).concat([root.val]).concat(right);
 }
 
 function postOrderArray(root) {
+    const result = [];
+
+    if (!root) return result;
+
+    const left = result.concat(postOrderArray(root.left));
+    const right = result.concat(postOrderArray(root.right));
+
+    return result.concat(left).concat(right).concat(root.val)
 
 }
 

0954e2315ada19c60d3a9dcfb2df91793f9c5f6f
Kzhao <ken.zh77@gmail.com>
2020-02-20 13:50:57 -0500

    finished stack queue

--- stack_queue_interview_problems/lib/3_stack_queue.js
+++ stack_queue_interview_problems/lib/3_stack_queue.js
@@ -82,18 +82,40 @@ class StackQueue {
         this.outStack = null;
         this.front = null;
         this.back = null;
+        this.length = 0
     }
 
-    enqueue(){
-
+    enqueue(val){
+        let newNode = new Node(val)
+        if (this.front === null) {
+            this.back = newNode
+            this.front = this.back
+        }else {
+            let temp = this.back 
+            this.back = newNode
+            temp.next = newNode
+        }
+        return ++this.length
     }
 
     dequeue(){
-
+        let temp = this.front;
+        if (this.length === 0) return null;
+        if (this.front === this.back) {
+            this.back = null;
+            this.front = null;
+            this.length --;
+            return temp;
+        }else {
+            this.front = this.front.next;
+            this.length --;
+            return temp;
+        }
+        
     }
 
     size(){
-        
+        return this.length
     }
 
 };

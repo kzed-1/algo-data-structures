631caad6ca5ae8fd83b57b3924c534b165e518f9
Kzhao <ken.zh77@gmail.com>
2020-03-04 10:33:11 -0500

    finished bst project

--- bst_project/lib/bst.js
+++ bst_project/lib/bst.js
@@ -8,6 +8,61 @@ class TreeNode {
 
 
 class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(val, root = this.root) {
+
+        if (!root) {
+            this.root = new TreeNode(val)
+            return;
+        }
+
+        if (val < root.val) {
+            if (!root.left) {
+                root.left = new TreeNode(val) 
+            }else {
+                this.insert(val, root.left);
+            }
+        } else {
+            if (!root.right) {
+                root.right = new TreeNode(val) 
+            }else {
+                this.insert(val, root.right);
+            }
+        }
+    }
+
+    searchRecur(val, root = this.root) {
+        if (!root) return false 
+
+        if (val < root.val) {
+            return this.searchRecur(val, root.left)
+        } else if (val > root.val) {
+            return this.searchRecur(val, root.right)
+        } else {
+            return true 
+        }
+
+        return false 
+    }
+
+    searchIter(val, root = this.root) {
+        if (!root) return false 
+
+        const queue = [root]
+
+        while (queue.length) {
+            const node = queue.shift()
+            if (node.val === val) return true
+            
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+        }
+
+        return false
+    }
    
 }
 
--- bst_project/lib/leet_code_108.js
+++ bst_project/lib/leet_code_108.js
@@ -2,6 +2,39 @@
 //  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
 
-function sortedArrayToBST(nums) {
+// Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
 
+// For this problem, a height - balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+// Example:
+
+// Given the sorted array: [-10, -3, 0, 5, 9],
+
+//     One possible answer is: [0, -3, 9, -10, null, 5], which represents the following height balanced BST:
+
+//       0
+//      / \
+//    -3   9
+//    /   /
+//  -10  5
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+}
+
+function sortedArrayToBST(nums) {
+    if (nums.length === 0) return null;
+    let midIdx = Math.floor(nums.length / 2)
+
+    let root = new TreeNode(nums[midIdx])
+
+    const leftSubTree = nums.slice(0, midIdx)
+    const rightSubTree = nums.slice(midIdx + 1)
+
+    root.left = sortedArrayToBST(leftSubTree)
+    root.right = sortedArrayToBST(rightSubTree)
+
+    return root
 }

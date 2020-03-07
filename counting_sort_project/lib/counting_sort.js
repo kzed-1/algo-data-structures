2752608817bf0f9ba4da4e4c463b1d18568c03b1
Kzhao <ken.zh77@gmail.com>
2020-03-07 15:37:19 -0500

    test

--- counting_sort_project/lib/counting_sort.js
+++ counting_sort_project/lib/counting_sort.js
@@ -1,20 +1,39 @@
-function countingSort(arr, max) {
-    const result = [];
-    const counters = new Array(max + 1).fill(0);
+// function countingSort(arr, max) {
+//     const result = [];
+//     const counters = new Array(max + 1).fill(0);
 
-    for(let i = 0; i < arr.length; i++){
-        counters[arr[i]]++;
+//     for(let i = 0; i < arr.length; i++){
+//         counters[arr[i]]++;
+//     }
+
+//     for(let i = 0; i < counters.length; i++){
+//         while(counters[i] > 0) {
+//             result.push(i);
+//             counters[i]--;
+//         }
+//     }
+
+//     return result;
+
+// }
+
+function countingSort(arr, max) {
+    if (arr.length <= 1) return arr;
+    let counters = new Array(max+1).fill(0);
+    let result = []
+
+    for (let i = 0; i < arr.length; i++) {
+        counters[arr[i]] ++;
     }
 
-    for(let i = 0; i < counters.length; i++){
-        while(counters[i] > 0) {
-            result.push(i);
-            counters[i]--;
+    for (let i = 0; i < counters.length; i++) {
+        while (counters[i] > 0) {
+            result.push(i)
+            counters[i] --;
         }
     }
 
-    return result;
-
+    return result
 }
 
 

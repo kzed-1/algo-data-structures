d48cfa6daea1461bebdb820bfff65ae0d6eec5b0
Kzhao <ken.zh77@gmail.com>
2020-03-15 15:08:52 -0400

    insertion

--- insertion_sort_project/lib/insertion_sort.js
+++ insertion_sort_project/lib/insertion_sort.js
@@ -1,15 +1,6 @@
 function insertionSort(arr) {
 
-    for (let i = 1; i < arr.length; i++) {
-        let currEle = arr[i];
-        let j = i - 1
-        for (j; j >= 0 && currEle < arr[j]; j--) {
-            arr[j + 1] = arr[j]
-        }
-        arr[j + 1] = currEle
-    } 
 
-    return arr
 }
 
 

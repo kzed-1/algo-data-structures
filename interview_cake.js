b314320625560ffe4d529b5b1142321c5193173e
Kzhao <ken.zh77@gmail.com>
2020-04-29 12:15:23 -0400

    merged array

--- interview_cake.js
+++ interview_cake.js
@@ -203,9 +203,47 @@ function reverseInPlace(arr) {
 
 
 
-const message = ['c', 'a', 'k', 'e', ' ',
-    'p', 'o', 'u', 'n', 'd', ' ',
-    's', 't', 'e', 'a', 'l'];
+// const message = ['c', 'a', 'k', 'e', ' ',
+//     'p', 'o', 'u', 'n', 'd', ' ',
+//     's', 't', 'e', 'a', 'l'];
 
-console.log(reverseInPlace(message))
+// console.log(reverseInPlace(message))
+
+// In order to win the prize for most cookies sold, 
+// my friend Alice and I are going to merge our Girl Scout Cookies orders and enter as one unit.
+
+// Each order is represented by an "order id"(an integer).
+
+// We have our lists of orders sorted numerically already, in arrays.
+// Write a function to merge our arrays of orders into one sorted array.
+
+// For example:
+
+const myArray = [3, 4, 6, 10, 11, 15];
+const alicesArray = [1, 5, 8, 12, 14, 19];
+
+// console.log(mergeArrays(myArray, alicesArray));
+// logs [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
+
+function mergeArrays(arr1, arr2) {
+
+    const merged = [];
+
+    const longestArr = (arr1.length > arr2.length) ? arr1.length : arr2.length
+
+    for (let i = 0; i < longestArr; i++) {
+        const ele1 = (arr1[0]) ? arr1[0] : Infinity
+        const ele2 = (arr2[0]) ? arr2[0] : Infinity
+    
+        let nextEle = (ele1 > ele2) ? ele2.shift() : ele1.shift()
+    
+        merged.push(nextEle)
+    }
+    
+    return merged
+    
+}
+
+console.log(mergeArrays(myArray, alicesArray));
+
 

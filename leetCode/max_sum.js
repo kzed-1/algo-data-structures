10e1eb83ef59241ef4a361e066e9605708c85a1c
ken zhao <ken.zhao42@gmail.com>
2020-05-15 14:15:16 -0400

    max subarray product

--- leetCode/max_sum.js
+++ leetCode/max_sum.js
@@ -13,3 +13,19 @@ var maxSubArray = function (nums) {
     return globalMax
 
 };
+
+var maxSubArray = function (nums) {
+
+    let localMax = nums[0];
+    let globalMax = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        localMax = Math.max(nums[i], localMax + nums[i])
+        if (localMax > globalMax) {
+            globalMax = localMax
+        }
+    }
+
+    return globalMax
+
+};

fecd4d19d0eb8c26419aeb9c93f47836a03c1c41
ken zhao <ken.zhao42@gmail.com>
2020-05-18 20:26:10 -0400

    find_search

--- leetCode/3sum.js
+++ leetCode/3sum.js
@@ -46,33 +46,3 @@ function sum3(nums) {
 };
 
 
-}
-
-
-
-let target = 0;
-nums.sort((a, b) => (a - b));
-let triplets = [];
-for (let i = 0; i < nums.length - 2; i++) {
-    if (i > 0 && nums[i] === nums[i - 1]) continue;
-    let left = i + 1;
-    let right = nums.length - 1;
-    while (left < right) {
-        let potentialMatch = nums[i] + nums[left] + nums[right];
-        if (potentialMatch === target) {
-            triplets.push([nums[i], nums[left], nums[right]]);
-            while (nums[left] === nums[left + 1]) left++;
-            while (nums[right] === nums[right - 1]) right--;
-            left += 1;
-            right -= 1;
-        }
-        else if (potentialMatch < target) {
-            left += 1;
-        }
-        else if (potentialMatch > target) {
-            right -= 1;
-        }
-    }
-}
-return triplets;
-};

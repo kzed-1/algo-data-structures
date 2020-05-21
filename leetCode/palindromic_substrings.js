6e30a89a07834410e3d55e1eb84533e58fb020a6
ken zhao <ken.zhao42@gmail.com>
2020-05-21 15:43:05 -0400

    find_search

--- leetCode/palindromic_substrings.js
+++ leetCode/palindromic_substrings.js
@@ -26,3 +26,38 @@
 
 // The input string length won't exceed 1000.
 
+var countSubstrings = function (s) {
+
+    let numPalStrs = 0;
+
+    if (s === null || s.length < 1) {
+        return 0
+    }
+
+
+    for (let i = 0; i < s.length; i++) {
+        let count1 = expandFromCenter(s, i, i);
+        let count2 = expandFromCenter(s, i, i + 1);
+
+        numPalStrs += count1 + count2
+    }
+
+    return numPalStrs
+
+
+};
+
+
+
+var expandFromCenter = function (s, i, j) {
+
+    let count = 0;
+
+    while (i >= 0 && j < s.length && s[i] === s[j]) {
+        count += 1;
+        i--;
+        j++;
+    }
+
+    return count
+}

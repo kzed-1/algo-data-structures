c051519d7613173fd8dfa0e203194b6ac9530fdd
ken zhao <ken.zhao42@gmail.com>
2020-06-08 12:27:58 -0400

    interview cake product execpt at that index

--- interview/product_except_at_index.js
+++ interview/product_except_at_index.js
@@ -33,8 +33,12 @@ function getProductsOfAllIntsExceptAtIndex(arr) {
 
     right[arr.length-1] = 1;
 
-    for (let i = 0; i < arr.length; i++) {
-        
+    for (let i = 1; i < arr.length; i++) {
+        left[i] = left[i-1] * arr[i-1]
+    }
+
+    for (let i = arr.length-2; i >= 0; i --) {
+        right[i] = right[i+1] * arr[i+1]
     }
 
 

791e74acb919cb898165653b898582e0d6d2ee2b
ken zhao <ken.zhao42@gmail.com>
2020-06-09 11:37:44 -0400

    cakeshop

--- interview/in_place_shuffle.js
+++ interview/in_place_shuffle.js
@@ -4,3 +4,39 @@
 
 // Assume that you have a function getRandom(floor, ceiling) for getting a random integer that is >= floor and <= ceiling.
 
+
+
+// iterate through array, and use get random function to generate a random number 
+// use set to hold numbers that have been seen 
+// if not in seen list use that number as index the new index 
+
+
+function shuffle(arr) {
+
+    const seen = new Set;
+
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        seen.add(i);
+
+        let newIdx;
+
+        while(seen.has(i)) {
+            newIdx = getRandom(0, arr.length)
+        };
+
+        result[newIdx] = arr[i]
+    }
+
+    return result;
+
+}
+
+function getRandom(floor, ceiling) {
+    return Math.floor(Math.random() * ceiling) + floor
+}
+
+const nums = [1,2,3,4,5]
+
+console.log(shuffle(num))

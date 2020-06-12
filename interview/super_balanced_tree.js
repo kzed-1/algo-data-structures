f87139e00a315f760a20d8b7a4163645a59cfe1a
ken zhao <ken.zhao42@gmail.com>
2020-06-12 12:32:56 -0400

    find duplicates

--- interview/super_balanced_tree.js
+++ interview/super_balanced_tree.js
@@ -22,3 +22,54 @@
 //     }
 // }
 
+
+// go through the tree
+// save an array for dept;
+// save an array for all the pairs of node and its depth
+// if we reach the end, if there is no depth, add to it
+// if a tree is unbalanced:
+// 1) if there are more than 2 depths;
+// 2) if ther are 2 depths and the difference is greater than 2;
+// return false if any of those scenarios are true 
+// other wise keep pushing left and right leaves 
+
+
+function superBalanced(treeRoot) {
+
+    const depths = [];
+    const pairs = [[treeRoot, 0]];
+
+    while (pairs) {
+        const pair = pairs.pop();
+        const node = pair[0];
+        const depth = pair[1];
+
+        if (!node.left && !node.right) {
+
+            if (depths.indexOf(depth) < 0) {
+                depths.push(depth);
+            } else {
+                if (depths.length > 2 || Math.abs(depths[0] - depths[1]) > 1) {
+                    return false 
+                }
+            }
+        } else {
+
+            if (node.left) {
+                pairs.push([node.left, depth+1])
+            }
+
+            if (node.right) {
+                pairs.push([node.right, depth+1])
+            }
+        }
+
+    }
+
+    return true;
+
+}
+
+console.log(superBalanced())
+
+

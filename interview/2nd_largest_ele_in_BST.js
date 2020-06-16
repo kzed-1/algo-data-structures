f730d4265707d04b9b62563cb527147ca920a934
ken zhao <ken.zhao42@gmail.com>
2020-06-16 13:12:19 -0400

    find duplicates

--- interview/2nd_largest_ele_in_BST.js
+++ interview/2nd_largest_ele_in_BST.js
@@ -20,21 +20,55 @@
 //     }
 // }
 
-function find2ndlargestEle(treeNode) {
+// function find2ndlargestEle(treeNode) {
 
-    let stack = [treeNode];
+//     let stack = [treeNode];
 
-    while (stack.length) {
-        const node = stack.pop();
+//     while (stack.length) {
+//         const node = stack.pop();
 
-        if (!node.right.right) {
-            return node
-        }
+//         if (!node.right.right) {
+//             return node
+//         }
 
-        if (node.right) {
-            stack.unshift(node.right)
-        }
+//         if (node.right) {
+//             stack.unshift(node.right)
+//         }
+//     }
 
+
+
+// }
+
+
+function findLargest(treeNode) {
+
+    if (!treeNode) {
+        return new Error('Tree must have at least 1 node');
     }
 
+    if (treeNode.right) {
+        return findLargest(treeNode);
+    }
+
+    return treeNode.value;
+
+}
+
+
+function findSecond(treeNode) {
+
+    if (!treeNode || (!treeNode.left && !treeNode.right)) {
+        return new Error('There must have at least 2 nodes');
+    }
+
+    if (treeNode.left && !treeNode.right) {
+        return findLargest(treeNode);
+    }
+
+    if (treeNode.right && !treeNode.right.left && !treeNode.right.right) {
+        return treeNode.value
+    }
+
+    return findSecond(treeNode.right)
 }
